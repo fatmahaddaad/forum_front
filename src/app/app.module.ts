65696d245927f03c6fc4ad07d4553d676233702f
Fatma Haddad <haddaadfatma@gmail.com>
2019-02-21 17:25:39 +0100

    created Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { ForumComponent } from './user/forum/forum.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserComponent,
+    AdminComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    ForumComponent
   ],
   imports: [
     BrowserModule,
