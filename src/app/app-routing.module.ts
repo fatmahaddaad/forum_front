0cb04bfdc4c1f8ab3b1497a3c2724bb17b9ea5e0
Fatma Haddad <haddaadfatma@gmail.com>
2019-02-25 14:37:30 +0100

    added `forum` route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { UserComponent } from './user/user.component';
 import { LoginComponent } from './user/login/login.component';
 import { registerContentQuery } from '@angular/core/src/render3';
 import { RegisterComponent } from './user/register/register.component';
+import { ForumComponent } from './user/forum/forum.component';
 
 const routes: Routes = [
   { path: 'login' , component: LoginComponent}, 
   { path: 'register', component: RegisterComponent},
+  { path: 'forum', component: ForumComponent},
 ];
 
 @NgModule({
