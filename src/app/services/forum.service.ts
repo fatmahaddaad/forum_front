776ac9c3cea4e51208c1ed5760aae3b02a1ff28b
Fatma Haddad <haddaadfatma@gmail.com>
2019-02-25 14:38:52 +0100

    added `login` functions

--- src/app/services/forum.service.ts
+++ src/app/services/forum.service.ts
@@ -8,12 +8,19 @@ export class ForumService {
 
   constructor(private http: Http) { }
   Url = 'http://127.0.0.1:8000/';
-
+  createAuthorizationHeader(headers: Headers) {
+    headers.append('Authorization', 'Bearer '+localStorage.getItem('token')); 
+  }
   createUser(user) {
     const formData: FormData = new FormData();
     formData.append('username', user.username);
     formData.append('email', user.email);
     formData.append('password', user.password);
     return this.http.post(this.Url+"register", formData);
+  }
+  loginUser(user) {
+    let headers = new Headers();
+    this.createAuthorizationHeader(headers);
+    return this.http.post(this.Url+`login_check`, user, {headers});
   }
 }
--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { NotifierService } from 'angular-notifier';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Http } from '@angular/http';
+import { ForumService } from '../../services/forum.service';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +11,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  private readonly notifier: NotifierService;
   username: string;
   password: string;
-  constructor() { }
+  constructor(private forumService: ForumService,
+    notifierService: NotifierService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private http: Http) {
+      this.notifier = notifierService;
+     }
 
   ngOnInit() {
   }
-
+  onLogin(){
+    const user ={
+      username : this.username,
+      password : this.password
+    }
+    if (user.username && !user.password) {
+      this.notifier.notify( 'error', 'password required' );
+    } else if (!user.username && user.password) {
+      this.notifier.notify( 'error', 'username required' );
+    } else if (user.password && user.username) {
+      this.forumService.loginUser(user).subscribe(res =>
+      {
+        localStorage.setItem('token', JSON.parse(res.text()).token);
+        this.notifier.notify( 'success', 'User Authentificated Successfully' );
+        this.router.navigate([`/forum/`]);
+      }, (err)=> {
+        this.notifier.notify( 'error', err.statusText );
+      });
+    } else {
+      this.notifier.notify( 'error', 'username and password required' );
+    }
+  }
 }
