b1157a9db690793f663043a496f25e42be8f2213
Fatma Haddad <haddaadfatma@gmail.com>
2019-02-28 15:50:43 +0100

    added `addReply`, `addComment` and `commentFormToggle`

--- src/app/services/forum.service.ts
+++ src/app/services/forum.service.ts
@@ -62,4 +62,20 @@ export class ForumService {
     formData.append('category_id', topic.category_id);
     return this.http.post(this.Url+"addTopic", formData, {headers});
   }
+  addReply(reply) {
+    let headers = new Headers();
+    this.createAuthorizationHeader(headers);
+    const formData: FormData = new FormData();
+    formData.append('content', reply.content);
+    formData.append('topic_id', reply.topic_id);
+    return this.http.post(this.Url+"addReply", formData, {headers});
+  }
+  addComment(comment) {
+    let headers = new Headers();
+    this.createAuthorizationHeader(headers);
+    const formData: FormData = new FormData();
+    formData.append('content', comment.content);
+    formData.append('reply_id', comment.reply_id);
+    return this.http.post(this.Url+"addComment", formData, {headers});
+  }
 }
--- src/app/user/forum/question/question.component.ts
+++ src/app/user/forum/question/question.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { NotifierService } from 'angular-notifier';
 import { ForumService } from '../../../services/forum.service';
 
 @Component({
@@ -8,11 +9,20 @@ import { ForumService } from '../../../services/forum.service';
   styleUrls: ['./question.component.css']
 })
 export class QuestionComponent implements OnInit {
+  private readonly notifier: NotifierService;
   topic: any;
   replies: any;
+  content: string;
+  comment_content: any[] = [];
+  MyClass: boolean[] = [];
   constructor(private forumService: ForumService,
     private route: ActivatedRoute,
-    private router: Router) { }
+    private router: Router,
+    notifierService: NotifierService,
+    private renderer: Renderer2, 
+    private el: ElementRef) { 
+      this.notifier = notifierService;
+    }
 
   ngOnInit() {
     let id = this.route.snapshot.paramMap.get('id');
@@ -22,9 +32,61 @@ export class QuestionComponent implements OnInit {
         this.forumService.getRepliesByTopic(id).subscribe(file => {
           this.replies = file.json();
           console.log(this.replies);
+          this.replies.forEach(element => {
+            this.MyClass[element.id]= true;
+          });
         });
       }
     });
+  }
+  onAddReply() {
+    let id = this.route.snapshot.paramMap.get('id');
+    const reply ={
+      content : this.content,
+      topic_id : id
+    }
+    console.log(reply);
+    this.forumService.addReply(reply).subscribe(res =>
+      {
+      this.notifier.notify( 'success', 'reply added successfully' );
+      this.forumService.getRepliesByTopic(id).subscribe(file => {
+        this.replies = file.json();
+      });
+      this.content = null;
+      }, (err) => {
+        console.log("err "+err);
+        this.notifier.notify( 'error', 'An error occurred while adding a new reply: ' + err.statusText );
+      });
+  }
+  onAddComment(reply_id, i) {
+    let id = this.route.snapshot.paramMap.get('id');
+    const comment ={
+      content : this.comment_content[i],
+      reply_id : reply_id
+    }
+    console.log(comment);
+    this.forumService.addComment(comment).subscribe(res =>
+      {
+      this.notifier.notify( 'success', 'comment added successfully' );
+      this.forumService.getRepliesByTopic(id).subscribe(file => {
+        this.replies = file.json();
+      });
+      this.comment_content[i] = null;
+      }, (err) => {
+        console.log("err "+err);
+        this.notifier.notify( 'error', 'An error occurred while adding a new comment: ' + err.statusText );
+    });
+  }
+  commentFormToggle(i) {
+    let element = document.getElementById('comment_form_'+i);
+    if(element.className.indexOf('hidden') !== -1)
+    {
+      this.MyClass[i]= false;
+    }
+    else
+    {
+      this.MyClass[i]= true;
+    }
   }
 
 }
