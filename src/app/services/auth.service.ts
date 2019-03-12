a0f199aec74fc408a955bb32c3c428dcb36e13c8
Fatma Haddad <haddaadfatma@gmail.com>
2019-03-12 11:42:56 +0100

    fixed issue

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,13 +8,14 @@ export class AuthService {
 
   constructor(public jwtHelper: JwtHelperService) { }
   public isAuthenticated(allowedRoles: string[]): boolean {
-    if (allowedRoles == null || allowedRoles.length === 0) {
-      return true;
-    }
+
     const token = localStorage.getItem('token');
     // Check whether the token is expired and return
     // true or false
     if (token) {
+      if (allowedRoles == null || allowedRoles.length === 0) {
+        return true;
+      }
       const decodeToken = this.jwtHelper.decodeToken(token);
       return !this.jwtHelper.isTokenExpired(token) && !allowedRoles.includes(decodeToken['roles']);
     }
